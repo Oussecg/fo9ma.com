653244fa106d1f7e9bb1758fa0b96b40a1a9fa3f
Oussecg <anasoussema16@gmail.com>
2025-11-20 22:09:39 +0100

    I add the random color with js !

---
+++ script.js
@@ -0,0 +1,31 @@
+// import $ from "jquery";
+
+class Operators {
+  constructor() {
+    this.randomColor.bind(this);
+    this.randomColor.bind(this);
+  }
+
+  randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  randomColor() {
+    const r = this.randomInt(0, 255);
+    const g = this.randomInt(0, 255);
+    const b = this.randomInt(0, 255);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  changeVariables() {
+    setInterval(() => {
+      const v = this.randomColor();
+      $('html').css("--background-color", v);
+    }, 1000);
+  }
+}
+
+const operators = new Operators;
+operators.changeVariables();
